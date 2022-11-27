fa265a7e76736ba73686ad144e305a2eeafc771c
prajwal1327 <118236029+prajwal1327@users.noreply.github.com>
2022-11-27 11:59:17 +0530

    Update sumoftwo.sh

--- sumoftwo.sh
+++ sumoftwo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Calculate the sum via command-line arguments
 # $1 and $2 refers to the first and second argument passed as command-line arguments
-sum=$(( $1 + $2 ))
-echo "Sum is: $sum"
+sum=$(( $1 * $2 ))
+echo "product is: $sum"
