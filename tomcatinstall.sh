11ce34afda671b00e69d951d5282182d5cb14ccd
prajwal1327 <118236029+prajwal1327@users.noreply.github.com>
2022-11-27 21:54:47 +0530

    Update tomcatinstall.sh

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -5,16 +5,16 @@ wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.2
 tar -xvzf apache-tomcat-10.0.27.tar.gz
 rm -rf apache-tomcat-10.0.27.tar.gz
 mv apache-tomcat-10.0.27 tomcat
-sh /opt/tomcat/bin/startup.sh
+sh tomcat/bin/startup.sh
 rm -rf conf-and-webapps-file
 git clone https://github.com/syedwaliuddin/conf-and-webapps-file.git
-rm -rf /opt/tomcat/conf/tomcat-users.xml
+rm -rf tomcat/conf/tomcat-users.xml
 cp conf-and-webapps-file/tomcat-users.xml /opt/tomcat/conf/
 sh tomcat/bin/startup.sh
-rm -rf /opt/tomcat/webapps/manager/META-INF/context.xml
+rm -rf tomcat/webapps/manager/META-INF/context.xml
 cp conf-and-webapps-file/context.xml /opt/tomcat/webapps/manager/META-INF/
-rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
+rm -rf tomcat/webapps/host-manager/META-INF/context.xml
 cp conf-and-webapps-file/contexthm.xml /opt/tomcat/webapps/host-manager/META-INF/
-sh /opt/tomcat/bin/startup.sh
+sh tomcat/bin/startup.sh
 rm -rf conf-and-webapps-file
 rm -rf apache-tomcat-10.0.27
